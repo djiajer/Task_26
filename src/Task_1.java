f21f001aa4e3974ed9f9141634fb0942840335c9
Svyatoslav Labza <newdjiajer@mail.ru>
2023-04-27 19:23:37 +0300

    Version 1.0

---
+++ src/Task_1.java
@@ -0,0 +1,25 @@
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+public class Task_1 {
+    public static boolean differenceBetweenLines() throws IOException {
+        Path path1 = Path.of("file1");
+        Path path2 = Path.of("file2");
+        Scanner scanner1 = new Scanner(path1);
+        Scanner scanner2 = new Scanner(path2);
+        boolean equal = false;
+
+        while (scanner1.hasNextLine() || scanner2.hasNextLine()) {
+            String line1 = scanner1.nextLine();
+            String line2 = scanner2.nextLine();
+            if (!line1.equals(line2)) {
+                equal = true;
+                System.out.println("Strings are not equal!\n" +
+                        "String from " + path1.getFileName() + ": \"" + line1 + "\"\n" +
+                        "String from " + path2.getFileName() + ": \"" + line2 + "\"\n");
+            }
+        }
+        return equal;
+    }
+}
