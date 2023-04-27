218ea5b2813fc1f4fe7bbc261405de6105ea9483
Svyatoslav Labza <newdjiajer@mail.ru>
2023-04-27 20:00:19 +0300

    Version 2.0

--- src/Task_1.java
+++ src/Task_1.java
@@ -3,6 +3,10 @@ import java.nio.file.Path;
 import java.util.Scanner;
 
 public class Task_1 {
+    public static void main(String[] args) throws IOException {
+        maxStringLength();
+    }
+
     public static boolean differenceBetweenLines() throws IOException {
         Path path1 = Path.of("file1");
         Path path2 = Path.of("file2");
@@ -20,6 +24,29 @@ public class Task_1 {
                         "String from " + path2.getFileName() + ": \"" + line2 + "\"\n");
             }
         }
+        scanner1.close();
+        scanner2.close();
         return equal;
     }
+    public static int maxStringLength() throws IOException {
+        Path path1 = Path.of("file1");
+        Scanner scanner1 = new Scanner(path1);
+
+        String maxLine = scanner1.nextLine();
+        String currentLine = null;
+        int maxLength = maxLine.length();
+        int currentLength;
+
+        while (scanner1.hasNextLine()) {
+            currentLine = scanner1.nextLine();
+            currentLength = currentLine.length();
+            if (currentLength > maxLength) {
+                maxLength = currentLength;
+                maxLine = currentLine;
+            }
+        }
+        System.out.println("Max length: " + maxLength + "\nMax line: " + maxLine);
+        return maxLength;
+    }
+
 }
