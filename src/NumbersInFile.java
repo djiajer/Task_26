2050154c2cf93b06910e5362163c9745a38fca5c
Svyatoslav Labza <newdjiajer@mail.ru>
2023-04-30 15:46:28 +0300

    Final version of task 3

--- src/NumbersInFile.java
+++ src/NumbersInFile.java
@@ -8,24 +8,28 @@ import java.util.Scanner;
 public class NumbersInFile {
     public static void main(String[] args) throws IOException {
         addNumbers();
+
     }
 
     public static void addNumbers() throws IOException {
         Path path = Path.of("numbers");
         Scanner scan = new Scanner(path);
-        //while(scan.hasNextLine()) {
+        List<List> lists = new ArrayList<>();
+        while(scan.hasNextLine()) {
             String line = scan.nextLine();
-            String[] nums = line.split("[\s,?!]+");
-            List<Integer> list = new ArrayList<>();
+            String[] nums = line.split("[\s,.?!]+");
+            List<Integer> numbers = new ArrayList<>();
             for (String num : nums) {
                 try {
                     int i = Integer.parseInt(num);
-                    list.add(i);
+                    numbers.add(i);
                 } catch (NumberFormatException e) {}
             }
-
-
-        System.out.println(list);
-        //}
+            lists.add(numbers);
+        }
+        Integer[][] result = new Integer[lists.size()][];
+        for (int i = 0; i < lists.size(); i++)
+            result[i] = (Integer[]) lists.get(i).toArray(new Integer[lists.get(i).size()]);
+        System.out.println(Arrays.deepToString(result));
     }
 }
